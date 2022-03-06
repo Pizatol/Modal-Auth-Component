80a906f8403654a8028ff7db6b5023a257245aa8
Pizatol <dev.belmonte@gmail.com>
2022-03-06 16:01:20 +0100

    mise en place formulaire d'auth

---
+++ pages/privateRoad.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Link from 'next/link'
+
+export default function PrivateRoad() {
+  return (
+    <>
+
+    <h1>Ceci est la page privée de test</h1>
+
+    <Link href="/">
+      <a>Retour</a>
+    </Link>
+
+    </>
+  )
+}
---
+++ pages/sign/signUp.js
@@ -0,0 +1,54 @@
+import React from "react";
+import css from "../../styles/signUp.module.scss";
+import Link from "next/link";
+
+export default function SignUp() {
+  return (
+    <>
+      <div className={css.globalContainer}>
+        <h1>Page Sign Up</h1>
+
+        <form className={css.form} method="post">
+          <h1>Log In</h1>
+          <div className={css.formEmail}>
+            <label for="email"> </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter you Email"
+              required
+            />
+          </div>
+          <div className={css.formPwd}>
+            <label for="pwd"></label>
+            <input
+              type="password"
+              name="pwd"
+              id="pwd"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className={css.formPwd}>
+            <label for="pwd"></label>
+            <input
+              type="password"
+              name="pwd"
+              id="pwd"
+              placeholder="Repeat password"
+              required
+            />
+          </div>
+          <div className={css.btnSubmit}>
+            <input type="submit" value="Subscribe!" />
+          </div>
+        </form>
+
+        <Link href="/">
+          <a> Retour</a>
+        </Link>
+      </div>
+    </>
+  );
+}
